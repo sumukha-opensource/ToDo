a88bafb4cb81317038debc43b01b24acd09d5744
sumukha <sumukh.aithal@gmail.com>
2022-11-12 23:50:49 +0530

    UI for add fragment

--- app/src/main/java/com/ksa/todo/MainActivity.kt
+++ app/src/main/java/com/ksa/todo/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.ksa.todo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?
+        navController = navHostFragment?.navController!!
+        setupActionBarWithNavController(let { navController })
+
+    }
+
+    override fun onNavigateUp(): Boolean {
+        //navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?)?.navController!!
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
